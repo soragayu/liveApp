e79347686dd9410cd67491408fadeba8610d20aa
槿原空 <sora@soranoMacBook-Pro.local>
2026-07-12 03:28:52 +0900

    7/12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,7 +5,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 export const hasTicket = async (eventId: number, memberId: number) => {
     const data = await sql`
     SELECT *
-    FROM livePerformers
+    FROM "livePerformers"
     WHERE "eventId" = ${eventId} AND "memberId" = ${memberId}
   `;
     return data.length > 0;
