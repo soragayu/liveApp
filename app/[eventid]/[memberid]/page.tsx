6f487dfe81a23442c3239e2e7ec966ed51b68bee
槿原空 <sora@soranoMacBook-Pro.local>
2026-07-12 04:16:16 +0900

    7/12

--- app/[eventid]/[memberid]/page.tsx
+++ app/[eventid]/[memberid]/page.tsx
@@ -23,7 +23,7 @@ const TicketPage = async ({ params }: PageProps) => {
         return (
             <div className="w-full h-screen flex justify-center items-center ">
                 <Image src={`/ticket${eventid}alt.png`} width={1080} height={1920} alt="ticket" className="w-full h-auto" />
-                <p className="text-[5vw] absolute bottom-[15vw] left-[13vw]">ナナヲアカリ</p>
+                <p className="text-[5vw] absolute bottom-[16vw] left-[13vw]">ナナヲアカリ</p>
             </div>
         )
     }
