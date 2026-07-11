0496ceb6d8d2f471b3f860af5c1c43d511e9b54b
槿原空 <sora@soranoMacBook-Pro.local>
2026-07-12 05:17:03 +0900

    7/12

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Darumadrop_One } from "next/font/google";
 import { Zen_Maru_Gothic } from "next/font/google";
 
 export const darumadrop = Darumadrop_One({ subsets: ["latin"], weight: "400" });
-export const zenmarugothic = Zen_Maru_Gothic({ subsets: ["latin"], weight: "400" });
+export const zenmarugothic = Zen_Maru_Gothic({ subsets: ["latin"], weight: ["400", "500", "700"] });
