25bcaddb580c5de5e73017f586488eb973434843
槿原空 <sora@soranoMacBook-Pro.local>
2026-07-23 15:13:12 +0900

    15:13

--- app/[eventid]/[memberid]/page.tsx
+++ app/[eventid]/[memberid]/page.tsx
@@ -14,14 +14,14 @@ const TicketPage = async ({ params }: PageProps) => {
         return (
             <div className="w-full h-screen flex justify-center items-center ">
                 <Image src="/bg.jpg" alt="bg" width={768} height={1376} className="fixed top-0 left-0 w-full" />
-                <Image src={`/ticket${eventid}.png`} width={1080} height={1920} alt="ticket" className="w-full h-auto animate-fade-in-up" />
+                <Image src={`/ticket${eventid}.png`} width={1080} height={1920} alt="ticket" className="w-full max-w-[768px] h-auto animate-fade-in-up" />
             </div>
         )
     } else {
         return (
             <div className="w-full h-screen flex justify-center items-center ">
                 <Image src="/bg.jpg" alt="bg" width={768} height={1376} className="fixed top-0 left-0 w-full" />
-                <Image src={`/ticket${eventid}alt.png`} width={1080} height={1920} alt="ticket" className="w-full h-auto animate-fade-in-up" />
+                <Image src={`/ticket${eventid}alt.png`} width={1080} height={1920} alt="ticket" className="w-full max-w-[768px] h-auto animate-fade-in-up" />
             </div>
         )
     }
